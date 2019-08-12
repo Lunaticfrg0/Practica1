2407b10af873a86d611296cc099b64803e410cb5
Emilio Escaño <emiliojoseescano@hotmail.com>
2019-08-12 18:34:05 -0400

    Primer push EN MI VIDA

---
+++ Practica1/Practica1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Practica1.Models;
+
+namespace Practica1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Practica1/Practica1/Views/AboutPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica1.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        void OpenBotton(object obj, EventArgs clicked)
+        {
+            string nombre = entNombre.Text;
+
+            if (string.IsNullOrEmpty(entNombre.Text) || string.IsNullOrEmpty(entPassword.Text))
+            {
+                DisplayAlert("Campo vacio!", " El campo de nombre y/o contraseña estan vacios ", "OK");
+               
+            }
+            else
+            {
+                DisplayAlert("Bienvenido", "Hola, te damos la bienvenida " + nombre + "!", "OK");
+            }
+        }
+    }
+}
